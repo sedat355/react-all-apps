8171c73f5d6c941f82149cfb7919a8eea216c368
Sedat Polat <sedatpolat355@gmail.com>
2024-06-18 17:03:09 +0300

    Birkaç düzeltme daha yapıldı

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 const Card = ({ card }) => {
   return (
     <div className="flex flex-col w-56 shadow-lg border">
-      <div className="mb-2">
-        <img className="h-62 w-62 p-2 object-cover" src={card.imageUrl} alt={card.title} />
+      <div className="mb-2 flex items-center justify-center">
+        <img className="max-w-56 object-cover" src={card.imageUrl} alt={card.title} />
       </div>
 
       <div className="p-4 flex flex-col h-full">
