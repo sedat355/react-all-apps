7f7f859e1c4129f30daeeeccb25d768c68e9d9db
Sedat Polat <sedatpolat355@gmail.com>
2024-06-18 16:47:18 +0300

    colors eklendi

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ export default {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-one': '#8AB9F1', 
+        'blue-two': '#73C2FB', 
+        'blue-three': '#1E90FF', 
+        'blue-four': '#1F75FE', 
+        'blue-five': '#0000FF',
+        'red-one': '#e63946', 
+        'orange-one': '#ff6700', 
+        'orange-two': '#e07a5f', 
+        'green-one': '#588157', 
+        'green-two': '#198754', 
+        'yellow-one': '#ffc300', 
+        'gray-one': '#adb5bd', 
+        'gray-two': '#778da9', 
+        'gray-three': '#415a77', 
+        'black-one': '#1b263b', 
+        'black-two': '#0d1b2a', 
+        'black-three': '#3d405b', 
+        'black-four': '#000814', 
+      }
+    },
   },
   plugins: [],
 }
