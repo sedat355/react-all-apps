788fb148f37b847cb4bf101a1cc94d7ff577da92
Sedat Polat <sedatpolat355@gmail.com>
2024-06-19 11:48:07 +0300

    Random animal getiriliyor

--- src/pages/AddAnimal.jsx
+++ src/pages/AddAnimal.jsx
@@ -1,7 +1,25 @@
 import bird from '../assets/svg/bird.svg'
 import cat from '../assets/svg/cat.svg'
+import cow from '../assets/svg/cow.svg'
+import dog from '../assets/svg/dog.svg'
+import gator from '../assets/svg/gator.svg'
+import horse from '../assets/svg/horse.svg'
 import Button from '../components/Button'
 
+const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+const animalsObject = {
+  bird, cat, cow, dog, gator, horse
+}
+
+console.log(animalsObject)
+
+const getRandomAnimal = (arr) => {
+  const randomIndex = Math.floor(Math.random() * (arr.length));
+  return arr[randomIndex]
+}
+
+getRandomAnimal(animals)
 
 const AddAnimal = () => {
   return (
@@ -10,7 +28,11 @@ const AddAnimal = () => {
         Add Animal
       </Button>
 
-      <div></div>
+      <ul className='flex mt-8'>
+        <li>
+          <img className='w-20 h-20' src={animalsObject[getRandomAnimal(animals)]} alt="bird" />
+        </li>
+      </ul>
     </div>
   )
 }
