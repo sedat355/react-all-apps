d4de9c12a9d67ad0ed26150e906b5a100ded4cef
Sedat Polat <sedatpolat355@gmail.com>
2024-06-19 21:57:40 +0300

    Kalplerin hepsi birden büyüyor

--- src/pages/AddAnimal.jsx
+++ src/pages/AddAnimal.jsx
@@ -5,17 +5,15 @@ import cow from '../assets/svg/cow.svg'
 import dog from '../assets/svg/dog.svg'
 import gator from '../assets/svg/gator.svg'
 import horse from '../assets/svg/horse.svg'
+import heart from '../assets/svg/heart.svg'
 import Button from '../components/Button'
 
 const animalsArray = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
-
 
 const getRandomAnimal = (arr) => {
   const randomIndex = Math.floor(Math.random() * (arr.length));
   return arr[randomIndex]
 }
-
-getRandomAnimal(animalsArray)
 
 const animalsObject = {
   bird, cat, cow, dog, gator, horse
@@ -23,11 +21,13 @@ const animalsObject = {
 
 const AddAnimal = () => {
   const [ animals, setAnimals ] = useState([]);
+  const [ clicks, setClicks ] = useState(1)
   
   console.log(animals)
-  
+
   const addAnimal = () => {
     const animal = getRandomAnimal(animalsArray)
+
     if(!animals.includes(animal)) {
       setAnimals([...animals, animal])
     } else if(animals.length !== animalsArray.length){
@@ -37,10 +37,16 @@ const AddAnimal = () => {
     }
   }
 
+  const handleHeartClick = (e) => {
+    console.log(e.target)
+    setClicks(clicks + 1)
+  }
+
   const renderedAnimals = animals.map( (animal, index) => {
     return(
       <li className='border p-2 rounded border-black-one' key={index}>
         <img className='h-20 w-20' src={animalsObject[animal]} alt={animal} />
+        <img onClick={handleHeartClick} src={heart} alt="heart" className={`cursor-pointer`} style={{width: 10 + 2*clicks + 'px'}}/>
       </li>
     )
   })
