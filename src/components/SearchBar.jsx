877327edda11aefa849f6d0001db784482d7b220
Sedat Polat <sedatpolat355@gmail.com>
2024-06-20 10:52:02 +0300

    Resimler SearchBar' a getirildi

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,36 @@
+import { useState } from "react"
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: 'Client-ID l9yV8CHo0MS7vxzSi6acS0tvwuWuJMA7PcpaFT8wdRU'
+    }
+  }
+
+  const baseUrl = "https://api.unsplash.com/search/photos?query=";
+
+  const fetchImages = async (term) => {
+    const response = await fetch(baseUrl + '/' + term, options);
+    const data = await response.json();
+
+    console.log(data.results)
+    
+
+    return data;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchImages(searchTerm);
+    setSearchTerm('')
+  }
+
   return (
-    <form className="w-full bg-yellow-one p-4">
-      <input type="text" placeholder="Bir terim girin.." />
+    <form onSubmit={handleSubmit} className="w-full bg-yellow-one p-4">
+      <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} type="text" placeholder="Bir terim girin.." />
     </form>
   )
 }
--- src/pages/Image-Gallery/ImageGallery.jsx
+++ src/pages/Image-Gallery/ImageGallery.jsx
@@ -1,9 +1,13 @@
+import { useEffect } from "react"
 import SearchBar from "../../components/SearchBar"
+import ImageList from "./ImageList"
 
 const ImageGallery = () => {
+
   return (
     <>
       <SearchBar />
+      <ImageList/>
     </>
   )
 }
