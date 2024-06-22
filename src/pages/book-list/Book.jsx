fb1ece8c204eb960a7928780036cdc886d63fb4f
Sedat Polat <sedatpolat355@gmail.com>
2024-06-22 10:17:08 +0300

    Kitap kartları render edildi

--- src/pages/book-list/Book.jsx
+++ src/pages/book-list/Book.jsx
@@ -1,6 +1,15 @@
-const Book = () => {
+const Book = ({book}) => {
+
   return (
-    <div>Book</div>
+    <li className="shadow-xl p-2">
+      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="photo" />
+      {/* <img src={`https://picsum.photos/300/200/?random`} alt="photo" /> */}
+      <span className="inline-block my-6">
+        <strong>Book Name:</strong> 
+        {' '}
+        {book.name}
+      </span>
+    </li>
   )
 }
 
--- src/pages/book-list/BookList.jsx
+++ src/pages/book-list/BookList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import BookForm from "./BookForm"
+import Book from "./Book";
 
 const BookList = () => {
   const [ books, setBooks ] = useState([]);
@@ -10,10 +11,18 @@ const BookList = () => {
     setBooks([...books, newBook ])
   }
 
+  const renderedBooks = books.map(book => {
+    return <Book key={book.id} book={book}/>
+  })
+
   return (
     <div className="w-full">
       <h1>Reading List</h1>
       <BookForm addBook={addBook}/>
+      
+      <ul className="mt-4 flex gap-2 flex-wrap">
+        {renderedBooks}
+      </ul>
     </div>
   )
 }
