62fd57cc0b99216ed01063b550d426aa72bfe50d
Sedat Polat <sedatpolat355@gmail.com>
2024-06-22 10:40:28 +0300

    Edit ve delete icon' ları eklendi

--- src/pages/book-list/Book.jsx
+++ src/pages/book-list/Book.jsx
@@ -1,9 +1,27 @@
+import { BiEditAlt } from 'react-icons/bi'
+import { BsTrash } from 'react-icons/bs'
+import Button from '../../components/Button'
+
 const Book = ({book}) => {
 
   return (
     <li className="shadow-xl p-2">
-      <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="photo" />
       {/* <img src={`https://picsum.photos/300/200/?random`} alt="photo" /> */}
+
+      <div className='relative'>
+        <img 
+          className="object-cover max-w-full" 
+          src={`https://picsum.photos/seed/${book.id}/300/200`} alt="photo" 
+        />
+
+        <Button size='circle' className='bg-gray-50 absolute top-2 right-10'>
+          <BiEditAlt className=''/>
+        </Button>
+
+        <Button size='circle' className='bg-gray-50 absolute top-2 right-2'>
+          <BsTrash/>
+        </Button>
+      </div>
       <span className="inline-block my-6">
         <strong>Book Name:</strong> 
         {' '}
