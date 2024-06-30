6cd809bdce2007d08413e37f6418cf522bdbe0cf
Sedat Polat <sedatpolat355@gmail.com>
2024-06-30 07:41:09 +0300

    Accordion aç/kapa çalışıyor

--- src/pages/accordion/AccordionItem.jsx
+++ src/pages/accordion/AccordionItem.jsx
@@ -1,19 +1,27 @@
 import { GoTriangleDown } from "react-icons/go"
-import Button from '../../components/Button'
+import { GoTriangleLeft } from "react-icons/go"
+import Button from "../../components/Button"
+import { useState } from "react"
 
 const AccordionItem = ({ item }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpenAccordion = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <div className="bg-gray-one flex flex-col px-3 py-2">
-      <div className="flex items-center justify-between">
+    <div className="bg-gray-one flex flex-col">
+      <div className="px-3 py-2 flex items-center justify-between">
         <h3>{item.title}</h3>
-        <Button btnType="outline" className="border-none px-0">
-          <GoTriangleDown className="text-3xl text-gray-three" />
+        <Button handleClick={handleOpenAccordion} btnType="outline" className="border-none px-0">
+          {
+            isOpen ? <GoTriangleDown className="text-3xl text-gray-three" /> : <GoTriangleLeft className="text-3xl text-gray-three" />
+          }
         </Button>
       </div>
 
-      <div className="pl-4 pt-1 pb-2">
-        {item.desc}
-      </div>
+      {isOpen && <div className="pl-8 pr-4 pt-1 pb-2 bg-gray-300">{item.desc}</div>}
     </div>
   )
 }
