9eafd1a80efba75be34f502dabba641e3b4d15f5
Sedat Polat <sedatpolat355@gmail.com>
2024-07-03 18:58:19 +0300

    modal açıkken scrolling durduruldu

--- src/pages/modal/Modal.jsx
+++ src/pages/modal/Modal.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 
 const Modal = ({ modalContent, handleCloseModal }) => {
+
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden')
+
+    return () => document.body.classList.remove('overflow-hidden')
+  },[])
 
   return ReactDOM.createPortal(
     <>
@@ -10,7 +17,7 @@ const Modal = ({ modalContent, handleCloseModal }) => {
 
       <div
         onClick={handleCloseModal}
-        className="overlay bg-black opacity-60 absolute inset-0"
+        className="overlay h-full bg-black opacity-60 fixed inset-0"
       ></div>
     </>,
     document.querySelector('.modal-container')
--- src/pages/modal/ModalPage.jsx
+++ src/pages/modal/ModalPage.jsx
@@ -16,29 +16,103 @@ const ModalPage = () => {
   const modalContent = (
     <>
       <p className="self-start">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
-          possimus officiis vero deleniti quos distinctio ipsa cupiditate rem
-          dolores voluptates! Voluptate quaerat possimus dolore enim blanditiis
-          ipsam quos perspiciatis error?
-        </p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+        possimus officiis vero deleniti quos distinctio ipsa cupiditate rem
+        dolores voluptates! Voluptate quaerat possimus dolore enim blanditiis
+        ipsam quos perspiciatis error?
+      </p>
 
-        <Button
-          handleClick={handleCloseModal}
-          btnType="danger"
-          className="self-end whitespace-nowrap"
-        >
-          I accept
-        </Button>
+      <Button
+        handleClick={handleCloseModal}
+        btnType="danger"
+        className="self-end whitespace-nowrap"
+      >
+        I accept
+      </Button>
     </>
   )
 
   return (
-    <div className="modalpage">
+    <div className="modalpage h-full">
       <Button btnType="primary" handleClick={handleOpenModal}>
         Open Modal
       </Button>
 
-      {isOpenModal && <Modal handleCloseModal={handleCloseModal} modalContent={modalContent} setIsOpenModal={setIsOpenModal} />}
+      {isOpenModal && (
+        <Modal
+          handleCloseModal={handleCloseModal}
+          modalContent={modalContent}
+          setIsOpenModal={setIsOpenModal}
+        />
+      )}
+
+      <div className="flex flex-col gap-y-8 mt-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit vitae
+          earum id asperiores reiciendis error ipsam illo vero amet! Corrupti,
+          quo magnam quia odit ullam accusantium ex non ipsam, id doloribus
+          aliquid numquam nesciunt quas asperiores amet maiores veritatis
+          dignissimos minus in! Qui suscipit, expedita veritatis reprehenderit
+          tempore a recusandae. Blanditiis et, repellendus alias placeat optio
+          nulla rerum minima porro voluptatibus consectetur quia ut animi
+          cumque? Eius velit libero dicta laboriosam dolores repellendus sit
+          unde cum nostrum sunt asperiores culpa incidunt cumque quia iure ex
+          eum illum ab ad ducimus, quidem, distinctio quaerat minus ea!
+          Quibusdam perspiciatis nesciunt consectetur.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit vitae
+          earum id asperiores reiciendis error ipsam illo vero amet! Corrupti,
+          quo magnam quia odit ullam accusantium ex non ipsam, id doloribus
+          aliquid numquam nesciunt quas asperiores amet maiores veritatis
+          dignissimos minus in! Qui suscipit, expedita veritatis reprehenderit
+          tempore a recusandae. Blanditiis et, repellendus alias placeat optio
+          nulla rerum minima porro voluptatibus consectetur quia ut animi
+          cumque? Eius velit libero dicta laboriosam dolores repellendus sit
+          unde cum nostrum sunt asperiores culpa incidunt cumque quia iure ex
+          eum illum ab ad ducimus, quidem, distinctio quaerat minus ea!
+          Quibusdam perspiciatis nesciunt consectetur.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit vitae
+          earum id asperiores reiciendis error ipsam illo vero amet! Corrupti,
+          quo magnam quia odit ullam accusantium ex non ipsam, id doloribus
+          aliquid numquam nesciunt quas asperiores amet maiores veritatis
+          dignissimos minus in! Qui suscipit, expedita veritatis reprehenderit
+          tempore a recusandae. Blanditiis et, repellendus alias placeat optio
+          nulla rerum minima porro voluptatibus consectetur quia ut animi
+          cumque? Eius velit libero dicta laboriosam dolores repellendus sit
+          unde cum nostrum sunt asperiores culpa incidunt cumque quia iure ex
+          eum illum ab ad ducimus, quidem, distinctio quaerat minus ea!
+          Quibusdam perspiciatis nesciunt consectetur.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit vitae
+          earum id asperiores reiciendis error ipsam illo vero amet! Corrupti,
+          quo magnam quia odit ullam accusantium ex non ipsam, id doloribus
+          aliquid numquam nesciunt quas asperiores amet maiores veritatis
+          dignissimos minus in! Qui suscipit, expedita veritatis reprehenderit
+          tempore a recusandae. Blanditiis et, repellendus alias placeat optio
+          nulla rerum minima porro voluptatibus consectetur quia ut animi
+          cumque? Eius velit libero dicta laboriosam dolores repellendus sit
+          unde cum nostrum sunt asperiores culpa incidunt cumque quia iure ex
+          eum illum ab ad ducimus, quidem, distinctio quaerat minus ea!
+          Quibusdam perspiciatis nesciunt consectetur.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed sit vitae
+          earum id asperiores reiciendis error ipsam illo vero amet! Corrupti,
+          quo magnam quia odit ullam accusantium ex non ipsam, id doloribus
+          aliquid numquam nesciunt quas asperiores amet maiores veritatis
+          dignissimos minus in! Qui suscipit, expedita veritatis reprehenderit
+          tempore a recusandae. Blanditiis et, repellendus alias placeat optio
+          nulla rerum minima porro voluptatibus consectetur quia ut animi
+          cumque? Eius velit libero dicta laboriosam dolores repellendus sit
+          unde cum nostrum sunt asperiores culpa incidunt cumque quia iure ex
+          eum illum ab ad ducimus, quidem, distinctio quaerat minus ea!
+          Quibusdam perspiciatis nesciunt consectetur.
+        </p>
+      </div>
     </div>
   )
 }
