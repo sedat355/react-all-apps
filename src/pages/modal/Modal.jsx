5924b9f1ebe2c646ae9916e4889a6eb79d497f83
Sedat Polat <sedatpolat355@gmail.com>
2024-07-03 18:08:33 +0300

    Modal blş. reusable olacak şekilde yeniden dizayn edildi

--- src/pages/modal/Modal.jsx
+++ src/pages/modal/Modal.jsx
@@ -1,36 +1,15 @@
-import Button from "../../components/Button"
 import ReactDOM from 'react-dom'
 
-const Modal = ({ setIsOpenModal }) => {
-  const handleClick = () => {
-    setIsOpenModal(false)
-  }
-
-  const handleOverlayClick = () => {
-    setIsOpenModal(false)
-  }
+const Modal = ({ modalContent, handleCloseModal }) => {
 
   return ReactDOM.createPortal(
     <>
       <div className="modal rounded-md w-96 h-60 bg-gray-one text-black-two p-4 flex fixed z-50 left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-        <p className="self-start">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
-          possimus officiis vero deleniti quos distinctio ipsa cupiditate rem
-          dolores voluptates! Voluptate quaerat possimus dolore enim blanditiis
-          ipsam quos perspiciatis error?
-        </p>
-
-        <Button
-          handleClick={handleClick}
-          btnType="danger"
-          className="self-end whitespace-nowrap"
-        >
-          I accept
-        </Button>
+        {modalContent}
       </div>
 
       <div
-        onClick={handleOverlayClick}
+        onClick={handleCloseModal}
         className="overlay bg-black opacity-60 absolute inset-0"
       ></div>
     </>,
--- src/pages/modal/ModalPage.jsx
+++ src/pages/modal/ModalPage.jsx
@@ -5,17 +5,40 @@ import Modal from "./Modal"
 const ModalPage = () => {
   const [isOpenModal, setIsOpenModal] = useState(false)
 
-  const handleClick = () => {
-    setIsOpenModal(!isOpenModal)
+  const handleOpenModal = () => {
+    setIsOpenModal(true)
   }
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false)
+  }
+
+  const modalContent = (
+    <>
+      <p className="self-start">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis,
+          possimus officiis vero deleniti quos distinctio ipsa cupiditate rem
+          dolores voluptates! Voluptate quaerat possimus dolore enim blanditiis
+          ipsam quos perspiciatis error?
+        </p>
+
+        <Button
+          handleClick={handleCloseModal}
+          btnType="danger"
+          className="self-end whitespace-nowrap"
+        >
+          I accept
+        </Button>
+    </>
+  )
 
   return (
     <div className="modalpage">
-      <Button btnType="primary" handleClick={handleClick}>
+      <Button btnType="primary" handleClick={handleOpenModal}>
         Open Modal
       </Button>
 
-      {isOpenModal && <Modal setIsOpenModal={setIsOpenModal} />}
+      {isOpenModal && <Modal handleCloseModal={handleCloseModal} modalContent={modalContent} setIsOpenModal={setIsOpenModal} />}
     </div>
   )
 }
