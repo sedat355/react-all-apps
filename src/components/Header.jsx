431bef4e1906548dc61cc175d22aa5bb5b4bc2ca
Sedat Polat <sedatpolat355@gmail.com>
2024-07-03 12:40:38 +0300

    active stiller ayarlandı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <nav className="mb-4 bg-blue-800 flex items-center justify-between px-4 py-2">
+    <nav className="header mb-4 bg-blue-800 flex items-center justify-between px-4 py-2">
       <h1 className="text-white">React All Apps</h1>
 
       <ul className="flex space-x-4">
