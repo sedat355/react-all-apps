f169cd721eb3eafbbe59c23c09ad5163284ffc6a
Sedat Polat <sedatpolat355@gmail.com>
2024-07-07 17:11:49 +0300

    Counter blş. useReducer ile yapıldı

--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,29 +1,58 @@
-import { useState } from "react"
+import { useReducer } from "react"
 import Button from "../components/Button"
 
-const CounterPage = () => {
-  const [count, setCount] = useState(0)
-  const [inputVal, setInputVal] = useState("")
+const ACTIONS = {
+  INCREMENT: "INCREMENT",
+  DECREMENT: "DECREMENT",
+  ADD_INPUT_VAL: "ADD_INPUT_VAL",
+  CHANGE_INPUT: "CHANGE_INPUT",
+}
 
-  console.log(inputVal)
+const counterReducer = (counter, action) => {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { ...counter, count: counter.count + 1 }
+    case ACTIONS.DECREMENT:
+      return { ...counter, count: counter.count - 1 }
+    case ACTIONS.CHANGE_INPUT:
+      return { ...counter, inputVal: action.payload }
+    case ACTIONS.ADD_INPUT_VAL:
+      return { ...counter, count: counter.count + Number(action.payload) }
+    default:
+      throw Error("Olay tanımlanamadı!!")
+  }
+}
+
+const initialState = {
+  count: 0,
+  inputVal: "",
+}
+
+const CounterPage = () => {
+  const [counter, dispatch] = useReducer(counterReducer, initialState)
 
   function increment() {
-    setCount(count + 1)
+    dispatch({ type: ACTIONS.INCREMENT })
   }
 
   function decrement() {
-    setCount(count - 1)
+    dispatch({ type: ACTIONS.DECREMENT })
   }
 
   function handleAddClick() {
-    setCount(count + Number(inputVal))
-    setInputVal("")
+    dispatch({ type: ACTIONS.ADD_INPUT_VAL, payload: counter.inputVal })
+    dispatch({ type: ACTIONS.CHANGE_INPUT, payload: "" })
+  }
+
+  function handleChange(e) {
+    const value = e.target.value
+    dispatch({ type: ACTIONS.CHANGE_INPUT, payload: value })
   }
 
   return (
     <div>
       <span className="text-xl">
-        Count: <strong className="text-2xl">{count}</strong>
+        Count: <strong className="text-2xl">{counter.count}</strong>
       </span>
 
       <div className="flex gap-x-2 mt-2">
@@ -37,8 +66,8 @@ const CounterPage = () => {
 
       <form className="flex gap-x-2 mt-4">
         <input
-          value={inputVal}
-          onChange={e => setInputVal(e.target.value)}
+          value={counter.inputVal}
+          onChange={handleChange}
           className="border border-black-one rounded py-1 px-2"
         />
         <Button handleClick={handleAddClick} className="" btnType="outline">
